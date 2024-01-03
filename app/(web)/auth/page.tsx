dceed3cabafe6822ca081770ef46e9f083e24d02
Drack112 <onepessoa3@gmail.com>
2024-01-03 15:32:54 -0300

    [FEAT] - Router para levar a página padrão caso já exista uma sessão

--- app/(web)/auth/page.tsx
+++ app/(web)/auth/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
-
 import { signUp } from "next-auth-sanity/client";
 import { signIn, useSession } from "next-auth/react";
 import toast from "react-hot-toast";
+import { useRouter } from "next/router";
 
 const defaultFormData = {
   email: "",
@@ -18,7 +18,7 @@ const Auth = () => {
   const [formData, setFormData] = useState(defaultFormData);
 
   const inputStyles =
-    "border border-gray-300 sm:text-sm text-black rounded:lg block w-full p-2.5 md:text-2xl";
+    "border border-gray-300 sm:text-sm text-black rounded-lg block w-full p-2.5 focus:outline-none";
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -26,14 +26,20 @@ const Auth = () => {
   };
 
   const { data: session } = useSession();
-  console.log(session);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) {
+      router.push("/");
+    }
+  }, [router, session]);
 
   const loginHandler = async () => {
     try {
       await signIn();
     } catch (error) {
       console.log(error);
-      toast.error("Something went wrong");
+      toast.error("Something wen't wrong");
     }
   };
 
@@ -42,13 +48,12 @@ const Auth = () => {
 
     try {
       const user = await signUp(formData);
-
       if (user) {
-        toast.success("Success. Please sign in.");
+        toast.success("Success. Please sign in");
       }
     } catch (error) {
       console.log(error);
-      toast.error("Something went wrong");
+      toast.error("Something wen't wrong");
     } finally {
       setFormData(defaultFormData);
     }
@@ -59,14 +64,14 @@ const Auth = () => {
       <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
         <div className="flex mb-8 flex-col md:flex-row items-center justify-between">
           <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl">
-            Create An Account
+            Create an account
           </h1>
           <p>OR</p>
           <span className="inline-flex items-center">
             <AiFillGithub
               onClick={loginHandler}
               className="mr-3 text-4xl cursor-pointer text-black dark:text-white"
-            ></AiFillGithub>
+            />{" "}
             |
             <FcGoogle
               onClick={loginHandler}
@@ -79,7 +84,6 @@ const Auth = () => {
           <input
             type="email"
             name="email"
-            id="email"
             placeholder="name@company.com"
             required
             className={inputStyles}
@@ -89,7 +93,6 @@ const Auth = () => {
           <input
             type="text"
             name="name"
-            id="name"
             placeholder="John Doe"
             required
             className={inputStyles}
@@ -99,7 +102,6 @@ const Auth = () => {
           <input
             type="password"
             name="password"
-            id="password"
             placeholder="password"
             required
             minLength={6}
@@ -107,6 +109,7 @@ const Auth = () => {
             value={formData.password}
             onChange={handleInputChange}
           />
+
           <button
             type="submit"
             className="w-full bg-tertiary-dark focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
@@ -116,7 +119,7 @@ const Auth = () => {
         </form>
 
         <button onClick={loginHandler} className="text-blue-700 underline">
-          Login
+          login
         </button>
       </div>
     </section>
