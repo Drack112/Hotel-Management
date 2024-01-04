80f518f1a61e8efbbe4a259b480ce08f6638ca7f
Drack112 <onepessoa3@gmail.com>
2024-01-04 08:39:06 -0300

    [FEAT] - Componente de pesquisa de quartos

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -1,5 +1,11 @@
 import HeroSection from "@/components/HeroSection/HeroSection";
+import PageSearch from "@/components/PageSearch/PageSearch";
 
 export default function Home() {
-  return <HeroSection />;
+  return (
+    <>
+      <HeroSection />
+      <PageSearch />
+    </>
+  );
 }
