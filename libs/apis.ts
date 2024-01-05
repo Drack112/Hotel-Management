e9f5124d2805ebf84c8a407dced7ebe25b5143a1
Drack112 <onepessoa3@gmail.com>
2024-01-05 13:18:38 -0300

    [FEAT] - Rotas da api do sanity para trabalhar com as queries de usuários

--- libs/apis.ts
+++ libs/apis.ts
@@ -2,6 +2,7 @@ import { CreateBookingDto, Room } from "@/models/Room";
 import sanityClient from "./sanity";
 import * as queries from "./sanityQueries";
 import axios from "axios";
+import { Booking } from "@/models/Booking";
 
 export async function getFeaturedRoom() {
   const result = await sanityClient.fetch(
@@ -93,3 +94,25 @@ export const updateHotelRoom = async (hotelRoomId: string) => {
 
   return data;
 };
+
+export async function getUserBookings(userId: string) {
+  const result = await sanityClient.fetch<Booking[]>(
+    queries.getUserBookingsQuery,
+    {
+      userId,
+    },
+    { cache: "no-cache" }
+  );
+
+  return result;
+}
+
+export async function getUserData(userId: string) {
+  const result = await sanityClient.fetch(
+    queries.getUserDataQuery,
+    { userId },
+    { cache: "no-cache" }
+  );
+
+  return result;
+}
